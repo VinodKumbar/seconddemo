b584e261e363114672630fc35c3afc4002f6891a
Vinod <vnkumbar@gmail.com>
2020-07-08 11:49:50 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void nativeApps() {
+		
+		System.out.println("Androis Native Apps");
+
+	}
+
+	@Test
+	public void iOSApps() {
+		
+		System.out.println("IOS Apps");
+
+
+	}
+}
